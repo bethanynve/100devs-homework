1704b68393a1e569755a4263d340aa21750b87b8
Bethany <26800016+bethanynve@users.noreply.github.com>
2022-05-02 20:10:02 -0400

    Complete exercises

--- thejsway_ch9.js
+++ thejsway_ch9.js
@@ -1,93 +1,96 @@
 // https://github.com/thejsway/thejsway/blob/master/manuscript/chapter09.md
 
+
 // Dogs
 // TODO: define the Dog class here
 
-// class Dog {
-//     constructor(name, species, size) {
-//         this.name = name;
-//         this.species = species;
-//         this.size = size;
-//     }
+class Dog {
+    constructor(name, species, size) {
+        this.name = name;
+        this.species = species;
+        this.size = size;
+    }
 
-//     bark() {
-//         if (this.size > 70) {
-//             return "Grrr! Grrr!";
-//         } else {
-//             return "Woof! Woof!";
-//         }
-//     }
-// }
+    bark() {
+        if (this.size > 70) {
+            return "Grrr! Grrr!";
+        } else {
+            return "Woof! Woof!";
+        }
+    }
+}
 
-// const fang = new Dog("Fang", "boarhound", 75);
-// console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
-// console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
 
-// const snowy = new Dog("Snowy", "terrier", 22);
-// console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
-// console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
 
 
 
 // Character inventory
 
-// class Character {
-//     constructor(name, health, strength, gold, keys) {
-//       this.name = name;
-//       this.health = health;
-//       this.strength = strength;
-//       this.xp = 0; // XP is always zero for new characters
-//       this.gold = 10;
-//       this.keys = 1;
-//     }
-//     // Attack a target
-//     attack(target) {
-//       if (this.health > 0) {
-//         const damage = this.strength;
-//         console.log(
-//           `${this.name} attacks ${target.name} and causes ${damage} damage points`
-//         );
-//         target.health -= damage;
-//         if (target.health > 0) {
-//           console.log(`${target.name} has ${target.health} health points left`);
-//         } else {
-//           target.health = 0;
-//           const bonusXP = 10;
-//           console.log(
-//             `${this
-//               .name} eliminated ${target.name} and wins ${bonusXP} experience points`
-//           );
-//           this.xp += bonusXP;
-//           this.gold += target.gold;
-//           this.keys += target.keys;
-//         }
-//       } else {
-//         console.log(`${this.name} can't attack (they've been eliminated)`);
-//       }
-//     }
-//     // Return the character description
-//     describe() {
-//       return `${this.name} has ${this.health} health points, ${this
-//         .strength} as strength, ${this.xp} XP points, ${this.gold} gold and ${this.keys} keys`;
-//     }
-//   }
-// const aurora = new Character("Aurora", 150, 25);
-// const glacius = new Character("Glacius", 130, 30);
+class Character {
+    constructor(name, health, strength, gold, keys) {
+      this.name = name;
+      this.health = health;
+      this.strength = strength;
+      this.xp = 0; // XP is always zero for new characters
+      this.inventory = {
+         gold: 10,
+         keys: 1
+      }
+    }
+    // Attack a target
+    attack(target) {
+      if (this.health > 0) {
+        const damage = this.strength;
+        console.log(
+          `${this.name} attacks ${target.name} and causes ${damage} damage points`
+        );
+        target.health -= damage;
+        if (target.health > 0) {
+          console.log(`${target.name} has ${target.health} health points left`);
+        } else {
+          target.health = 0;
+          const bonusXP = 10;
+          console.log(
+            `${this
+              .name} eliminated ${target.name} and wins ${bonusXP} experience points`
+          );
+          this.xp += bonusXP;
+          this.inventory.gold += target.inventory.gold;
+          this.inventory.keys += target.inventory.keys;
+        }
+      } else {
+        console.log(`${this.name} can't attack (they've been eliminated)`);
+      }
+    }
+    // Return the character description
+    describe() {
+      return `${this.name} has ${this.health} health points, ${this
+        .strength} as strength, ${this.xp} XP points, ${this.inventory.gold} gold and ${this.inventory.keys} key(s)`;
+    }
+  }
+const aurora = new Character("Aurora", 150, 25);
+const glacius = new Character("Glacius", 130, 30);
 
-// console.log("Welcome to the adventure! Here are our heroes:");
-// console.log(aurora.describe());
-// console.log(glacius.describe());
+console.log("Welcome to the adventure! Here are our heroes:");
+console.log(aurora.describe());
+console.log(glacius.describe());
 
-// const monster = new Character("Spike", 40, 20);
-// console.log("A wild monster has appeared: it's named " + monster.name);
+const monster = new Character("Spike", 40, 20);
+console.log("A wild monster has appeared: it's named " + monster.name);
 
-// monster.attack(aurora);
-// monster.attack(glacius);
-// aurora.attack(monster);
-// glacius.attack(monster);
+monster.attack(aurora);
+monster.attack(glacius);
+aurora.attack(monster);
+glacius.attack(monster);
 
-// console.log(aurora.describe());
-// console.log(glacius.describe());
+console.log(aurora.describe());
+console.log(glacius.describe());
 
 
 
@@ -102,20 +105,16 @@ class Account {
         return this.balance += value;
     }
     describe() {
-        return `The bank account belonging to ${this.name} has a balance of ${this.balance}.`;
+        return `owner: ${this.name}, balance: ${this.balance}`;
     }
 }
 
-const sean = new Account("Sean", 0);
-const brad = new Account("Brad", 0);
-const georges = new Account("Georges", 0);
+const accountArr = [];
+accountArr.push(new Account("Sean"));
+accountArr.push(new Account("Brad"));
+accountArr.push(new Account("Georges"));
 
-sean.credit(1000);
-brad.credit(1000);
-georges.credit(1000);
-
-console.log(sean.describe());
-console.log(brad.describe());
-console.log(georges.describe());
-
- 
+accountArr.forEach(account => {
+    account.credit(1000);
+    console.log(account.describe());
+})
